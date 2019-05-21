26b903e8e40f3ad9fd7f55052c5fbc5570c664c9
mahaveer0496 <mahaveersharma0496@yahoo.com>
2019-05-22 01:29:38 +0530

    Setup babel node and nodemon

---
+++ index.js
@@ -0,0 +1,18 @@
+import {GraphQLServer} from 'graphql-yoga'
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, {name}) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({typeDefs, resolvers})
+server.start(() =>
+  console.log('GraphQL is running on http://localhost:4000'),
+)
