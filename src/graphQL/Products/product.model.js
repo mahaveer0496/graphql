3bb7fef6372f817cc63a77e8c5396623d8fbe50d
mahaveer0496 <mahaveersharma0496@yahoo.com>
2019-05-27 22:04:42 +0530

    Add Coupon schma and model update produc shema and model

--- src/graphQL/Products/product.model.js
+++ src/graphQL/Products/product.model.js
@@ -1,15 +1,59 @@
 import {Schema, model} from 'mongoose'
 
 const categoryEnum = ['Electronics', 'Clothes', 'Furniture']
-const productSchema = new Schema({
-  name: {type: String, required: true},
-  price: {type: Number, required: true},
-  category: {
-    type: String,
-    enum: categoryEnum,
-    required: true,
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: 'https://via.placeholder.com/150',
+      validate: [
+        (v) => validator.isURL(v),
+        'Not a valid image url',
+      ],
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ['GAMING_PC', 'BIKE', 'DRONE'],
+    },
+    description: String,
+    liquidCooled: {
+      type: Boolean,
+      required() {
+        return this.type === 'GAMING_PC'
+      },
+    },
+    bikeType: {
+      type: String,
+      enum: ['KIDS', 'MOUNTAIN', 'ELECTRIC', 'BEACH'],
+      required() {
+        return this.type === 'BIKE'
+      },
+    },
+    range: {
+      type: String,
+      required() {
+        return this.type === 'DRONE'
+      },
+    },
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'user',
+    },
   },
-  description: String,
-})
+  {timestamps: true},
+)
 
 export default model('Product', productSchema)
